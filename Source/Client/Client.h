80087a1203af22d14920eaf6f1a0fbc43eb291f7
Oleksandr Bondar <aleksadnrr.bondar@gmail.com>
2025-03-15 14:35:48 +0200

    refactored client code

--- Source/Client/Client.h
+++ Source/Client/Client.h
@@ -19,6 +19,12 @@ public:
     void sendData(const std::string& type, const std::string& data);
 
 private:
+    void sendText(const std::string& text);
+    void sendHeader(const std::string& type);
+    void sendFile(const std::string& filePath);
+    void sendFileExtension(const std::string& filePath);
+    void sendFileContent(std::ifstream& file, size_t fileSize);
+
     static size_t getFileSize(std::ifstream& file);
 
 private:
--- Source/Client/Main.cpp
+++ Source/Client/Main.cpp
@@ -5,6 +5,9 @@ int main()
     Client client("127.0.0.1", "12345");
 
     client.sendData("TEXT", "Hello, Server!");
+    client.sendData("FILE", "C:\\Users\\aleks\\Downloads\\DS (2).ico");
+    client.sendData("FILE", "C:\\Users\\aleks\\Desktop\\Data.mp4");
+    client.sendData("FILE", "C:\\Users\\aleks\\Downloads\\VulkanSDK-1.4.304.1-Installer.exe");
 
     return 0;
 }
